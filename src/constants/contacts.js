a196dac34ef435a9bf37cc439968a76141889268
Oleksandr-Kalnaus <oleksandrkalnaus@gmail.com>
2024-11-30 22:19:16 +0000

    added all scripts

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,4 +1,4 @@
-import { Path } from './../../node_modules/@humanfs/core/src/path';
+import path from 'node:path';
 
-export const PATH_DB = Path.resolve('src', 'db', 'db.json');
+export const PATH_DB = path.resolve('src', 'db', 'db.json');
 console.log(PATH_DB);
